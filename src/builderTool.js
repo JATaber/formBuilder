e83d7c3cd954d71732494a2c2b6cac082277d4db
XALBIA-PC\james <JATaber@fullsail.edu>
2018-05-05 10:23:24 -0500

    impleminted airBnB style completely

--- src/builderTool.js
+++ src/builderTool.js
@@ -1,3 +1,7 @@
+// Error message template that will be thrown when an error occurs
+const error = (message, value) => {
+  throw new Error(`FormBuilder - ${message}\nYour input:\n${value}`);
+};
 // Factory for building different input types
 const inputFactory = (input) => {
   // is the array exists form the form
@@ -9,89 +13,63 @@ const inputFactory = (input) => {
         if (input.name && input.options && Array.isArray(input.options)) {
           // Builds a string to return
           let outputString = `<select name="${input.name}">\n`;
-          for (let i = 0; i < input.options.length; i++){
+          for (let i = 0; i < input.options.length; i += 1) {
             outputString += `<option value="${input.options[i].value}">${input.options[i].label}</option>\n`;
           }
-          
-          //close the select tag
+          // close the select tag
           outputString += '</select>\n';
-          
-          //this labels the select element of the form
-          if (input.label){
+          // this labels the select element of the form
+          if (input.label) {
             outputString = `<label>${input.label} ${outputString}</label>\n`;
           }
-          
           outputString = `${outputString}`;
-          
           return outputString;
-        }else{
-          //this displays the error that is thrown
-          return error('The select inputs require a name string and options array.', [input.name, input.options]);
         }
-        break;
-      
+        // this displays the error that is thrown
+        return error('The select inputs require a name string and options array.', [input.name, input.options]);
       case 'label':
         // Check if value or throw exception
-        if (input.value){
+        if (input.value) {
           return `<label>${input.value}</label>\n`;
-        }else{
-          return error('The label inputs require a value string.', input.value);
         }
-        break;
-      
+        return error('The label inputs require a value string.', input.value);
       case 'reset':
-        if(input.type === 'reset'){
+        if (input.type === 'reset') {
           return `<input type="${input.type}" value="${input.value}">\n`;
-        }else{
-          return error(`The ${input.type} inputs require a value string`, input.value);
         }
-        break;
-      
+        return error(`The ${input.type} inputs require a value string`, input.value);
       case 'submit':
-        if(input.type === 'submit'){
-          return `<input type="${input.type}" value="${input.value}">\n`
-        }else{
-          return error(`The ${input.type} inputs require a value string`, input.value);
+        if (input.type === 'submit') {
+          return `<input type="${input.type}" value="${input.value}">\n`;
         }
-        break;
-      
+        return error(`The ${input.type} inputs require a value string`, input.value);
       default:
-        if (input.name){
-          
+        if (input.name) {
           // Builds a input string to return
           let outputString = `<input type="${input.type}" name="${input.name}"`;
-          
-          //checks to see if a value has been entered and
-          if (input.value){
+          // checks to see if a value has been entered and
+          if (input.value) {
             outputString += ` value="${input.value}"/>`;
-          }else{
-            //this closes the input field
+          } else {
+            // this closes the input field
             outputString += ' />\n';
           }
-          
-          if (input.label){
+          if (input.label) {
             outputString = `<label>${input.label} ${outputString}</label>\n`;
           }
-          
           return outputString;
-        }else{
-          return error(`The ${input.type} inputs require a value string`, input.value);
         }
+        return error(`The ${input.type} inputs require a value string`, input.value);
     }
   }
-  
   return error('You must supply a valid input type.', input.type);
-};
-// Error message template that will
-const error = (message, value) => {
-  throw new Error(`FormBuilder - ${message}\nYour input:\n${value}`);
 };
 // Main function to build the complete form
 module.exports.buildForm = (inputs, action = false, method = 'GET') => {
   // create a variable to build the form string
   let outputString = '<form';
   // this checks to see if an action was set
-  if (action){
+  if (action) {
     outputString += ` action="${action}"`;
   }
   // this checks to see if a method was set
